fa9da61f60e2f9b9024e5dcc76d3256d2e1d48db
Paul @ PC <paul.filip@protonmail.de>
2021-02-08 13:24:27 +0100

    Start corrections on 04

--- 04-LOW-TEMPERATURES/script/ex1.py
+++ 04-LOW-TEMPERATURES/script/ex1.py
@@ -48,15 +48,6 @@ model = linear_R(linear_x_cu,debye_T, debye_R)
 T_copper, R_copper = debye_T, debye_R
 T_copper_err, R_copper_err = np.sqrt(pcov[0][0]), np.sqrt(pcov[1][1])
 
-specific_resistance_300 = 0.017e-6 # Ohm meter
-l_copper = 2. * 7.8642e-9/specific_resistance_300
-l_copper_err = 0.0005 * 7.8642e-9/specific_resistance_300
-specific_resistivity = 658.7e-18
-
-# mean_free_path =
-
-print(l_copper)
-
 print(f"Copper: T_debye = {debye_T:.3f} +- {np.sqrt(pcov[0][0]):.3f} K")
 print(f"Copper: R_debye = {debye_R:.3f} +- {np.sqrt(pcov[1][1]):.3f} Ohm")
 
@@ -77,14 +68,31 @@ for i in range(len(fit_err_slope)):
     gradient = np.array([d_ds[i],d_do[i]])
     fit_err_slope[i] += np.sqrt( gradient.T @ pcov_slope @ gradient)
 
+# calculate resistivity and mean free path
+
+resistance_300 = 2.163 # Ohms
+resistance_42 = np.exp(offset) * 18**slope
+resistance_42_err = np.sqrt( resistance_42**2 * (pcov_slope[1][1]**2 + 4.2/slope * pcov_slope[0][0]**2 ) )
+specific_resistance_300 = 0.017e-6 # Ohm meter
+l_copper = resistance_300 * 7.8642e-9/specific_resistance_300 # meters
+l_copper_err = 0.0005 * 7.8642e-9/specific_resistance_300
+rho_cu = resistance_42 * 7.8642e-9/l_copper
+rho_cu_err = np.sqrt( (rho_cu * resistance_42_err/resistance_42)**2 + (-rho_cu * l_copper_err/l_copper)**2 )
+rhol_cu = 658.7e-18 # Ohm meter²
+mean_free_path_cu = rhol_cu/rho_cu # meters
+mean_free_path_cu_err = mean_free_path_cu * rho_cu_err/rho_cu
+
+print(f"Copper: Resistivity 4.2 K: rho = {rho_cu:.1e} +- {rho_cu_err:.1e} Ohm * m")
+print(f"Copper: Mean free path: mu = {mean_free_path_cu:.1e} +- {mean_free_path_cu_err:.1e} m")
+
 # plotting everything
-plt.plot(linear_x_cu,model,c="C1",label="best fits")
-plt.plot(nonlinear_x_cu, model_slope,c="C1")
-plt.plot(np.linspace(50,280,10),linear_R(np.linspace(50,280,10),343,debye_R)+0.09,c="gray",ls="--",label="expected")
-plt.fill_between(nonlinear_x_cu,0.01*model_slope-fit_err_slope, 3*model_slope+fit_err_slope,alpha=0.2,color="C0")
-plt.fill_between(linear_x_cu,model-fit_err+0.8, model+fit_err-0.8,alpha=0.2,color="C0",label="fit + 1$\sigma$")
-plt.errorbar(linear_x_cu, linear_y_cu,capsize=3,xerr=2,yerr=0.0005,c="C0",ls="none",label="measured data")
-plt.errorbar(nonlinear_x_cu, nonlinear_y_cu,xerr=2,yerr=0.0005,capsize=3,c="C0",ls="none")
+# plt.plot(linear_x_cu,model,c="C1",label="best fits")
+# plt.plot(nonlinear_x_cu, model_slope,c="C1")
+# plt.plot(np.linspace(50,280,10),linear_R(np.linspace(50,280,10),343,debye_R)+0.09,c="gray",ls="--",label="expected")
+# plt.fill_between(nonlinear_x_cu,0.01*model_slope-fit_err_slope, 3*model_slope+fit_err_slope,alpha=0.2,color="C0")
+# plt.fill_between(linear_x_cu,model-fit_err+0.8, model+fit_err-0.8,alpha=0.2,color="C0",label="fit + 1$\sigma$")
+# plt.errorbar(linear_x_cu, linear_y_cu,capsize=3,xerr=2,yerr=0.0005,c="C0",ls="none",label="measured data")
+# plt.errorbar(nonlinear_x_cu, nonlinear_y_cu,xerr=2,yerr=0.0005,capsize=3,c="C0",ls="none")
 
 ## NIOBIUM ##
 
@@ -127,6 +135,21 @@ for i in range(len(fit_err_slope)):
     gradient = np.array([d_ds[i],d_do[i]])
     fit_err_slope[i] += np.sqrt( gradient.T @ pcov_slope @ gradient)
 
+# calculate resistivity and mean free path
+
+resistance_300 = 56.59 # Ohms
+resistance_12 = np.exp(offset) * 12**slope
+resistance_12_err = np.sqrt( resistance_12**2 * (pcov_slope[1][1]**2 + 4.2/slope * pcov_slope[0][0]**2 ) )
+l_niobium = 8e-3 # meters
+rho_nb = resistance_12 * 3.6e-11/l_niobium
+rho_nb_err = rho_nb * resistance_12_err/resistance_12
+rhol_nb = 375e-18 # Ohm meter²
+mean_free_path_nb = rhol_nb/rho_nb # meters
+mean_free_path_nb_err = mean_free_path_nb * rho_nb_err/rho_nb
+
+print(f"Niobium: Resistivity 12 K: rho = {rho_nb:.1e} +- {rho_nb_err:.1e} Ohm * m")
+print(f"Niobium: Mean free path: mu = {mean_free_path_nb:.1e} +- {mean_free_path_nb_err:.1e} m")
+
 # # plotting everything
 # plt.figure()
 # plt.plot(linear_x_nb,model,c="C1",label="best fits")
@@ -143,9 +166,6 @@ for i in range(len(fit_err_slope)):
 # plt.errorbar(T_down[:-4],U_SLP[:-4],xerr=2,yerr=0.005,ls="none",c="C0")
 # plt.errorbar(T_up,U_SLP_up,c="C0",xerr=2,yerr=0.005,ls="none")
 # plt.yscale("log")
-# plt.xlabel("Temperature (K)")
-# plt.ylabel("Resistivity (m$\Omega$)")
-# plt.legend()
 
 ## COMBINED ANALYSIS ##
 
@@ -176,12 +196,16 @@ print()
 print(f"COMBINED ANALYSIS: slope = {slope:.3f} +- {slope_err:.3f}")
 print(f"COMBINED ANALYSIS: offset = {offset:.3f} +- {offset_err:.3f}")
 
-plt.errorbar(X,Y,X_err,Y_err,ls="none")
-plt.scatter(linear_x_cu/T_copper, linear_y_cu/R_copper,marker="s",s=5,label="Copper")
-plt.scatter(linear_x_nb/T_niobium, linear_y_nb/R_niobium,marker="o",s=10,zorder=50,label="Niobium")
-plt.fill_between(X,model-fit_err, model+fit_err,alpha=0.2,color="C0",label="fit + 1$\sigma$")
-plt.legend()
-plt.xlabel(r"T / $\theta$")
-plt.ylabel(r"R / R$_\theta$")
+# plt.errorbar(X,Y,X_err,Y_err,ls="none")
+# plt.scatter(linear_x_cu/T_copper, linear_y_cu/R_copper,marker="s",s=5,label="Copper")
+# plt.scatter(linear_x_nb/T_niobium, linear_y_nb/R_niobium,marker="o",s=10,zorder=50,label="Niobium")
+# plt.fill_between(X,model-fit_err, model+fit_err,alpha=0.2,color="C0",label="fit + 1$\sigma$")
+# plt.legend()
+# plt.xlabel(r"T / $\theta$")
+# plt.ylabel(r"R / R$_\theta$")
+
+# plt.xlabel("Temperature (K)")
+# plt.ylabel("Resistance ($\Omega$)")
+# plt.legend()
 
 plt.show()
